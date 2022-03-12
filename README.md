241e2c3dcb21b3f9a81821a24d78c7df58e64805
sameerabegum <sameera11092@gmail.com>
2022-03-12 17:48:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yxqcht
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yxqcht)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+const evenOdd = (userInput) => {
+  if (userInput % 2 == 0) {
+    console.log('Its even');
+  } else {
+    console.log('its odd');
+  }
+};
+evenOdd(4);
